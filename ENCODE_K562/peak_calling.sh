6bc6540f51fd9c0b691f805b656bde75fbbf9599
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-03-10 13:20:45 +0000

    created peak_calling.sh file
    Homer peak calling in Terminal

---
+++ ENCODE_K562/peak_calling.sh
@@ -0,0 +1,8 @@
+## Using Homer for peak calling
+
+# create tag directory for each bam file (all on one line)
+
+makeTagDirectory chip/ /ifs/research-groups/botnar/proj013/src/ENCODE_MLLT1_K562/deduplicated/input_1.bam 
+/ifs/research-groups/botnar/proj013/src/ENCODE_MLLT1_K562/deduplicated/input_2.bam
+/ifs/research-groups/botnar/proj013/src/ENCODE_MLLT1_K562/deduplicated/IP_1.bam
+/ifs/research-groups/botnar/proj013/src/ENCODE_MLLT1_K562/deduplicated/IP_2.bam
