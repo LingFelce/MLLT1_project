7b7ad1561a228d008d76cb6f3da06c84030665f1
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-03-11 14:22:30 +0000

    created peak_calling file

---
+++ GSE117864/peak_calling.sh
@@ -0,0 +1,12 @@
+# DOT1L inhibition reveals a distinct subset of enhancers dependent on H3K79 methylation (ChIP-seq)
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE117864
+# get SRR number for each sample, and type into ebi.ac.uk to download eg https://www.ebi.ac.uk/ena/data/view/SRR7620816
+# SRR7620816 input_DMSO
+# SRR7620817 IP_DMSO
+# SRR7620818 input_EPZ (DOT1L inhibitor)
+# SRR7620819 IP_EPZ 
+
+# downloaded fastq.gz files from ebi
+sh download.sh
+
+
