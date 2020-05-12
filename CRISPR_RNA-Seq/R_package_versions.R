b70b324b68065a63341a91de2299ed00265bd307
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-05-12 12:28:46 +0100

    on laptop

---
+++ CRISPR_RNA-Seq/R_package_versions.R
@@ -0,0 +1,38 @@
+> sessionInfo()
+R version 4.0.0 (2020-04-24)
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+Running under: Windows 10 x64 (build 17134)
+
+Matrix products: default
+
+Random number generation:
+ RNG:     Mersenne-Twister 
+ Normal:  Inversion 
+ Sample:  Rounding 
+ 
+locale:
+[1] LC_COLLATE=English_United Kingdom.1252  LC_CTYPE=English_United Kingdom.1252    LC_MONETARY=English_United Kingdom.1252
+[4] LC_NUMERIC=C                            LC_TIME=English_United Kingdom.1252    
+
+attached base packages:
+[1] parallel  stats4    stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+[1] org.Hs.eg.db_3.11.1    AnnotationDbi_1.50.0   IRanges_2.22.1         S4Vectors_0.26.0       Biobase_2.48.0        
+[6] BiocGenerics_0.34.0    clusterProfiler_3.16.0 DOSE_3.14.0           
+
+loaded via a namespace (and not attached):
+ [1] viridis_0.5.1       httr_1.4.1          tidyr_1.0.3         tidygraph_1.1.2     bit64_0.9-7         jsonlite_1.6.1     
+ [7] viridisLite_0.3.0   splines_4.0.0       ggraph_2.0.2        assertthat_0.2.1    DO.db_2.9           BiocManager_1.30.10
+[13] rvcheck_0.1.8       triebeard_0.3.0     urltools_1.7.3      blob_1.2.1          progress_1.2.2      ggrepel_0.8.2      
+[19] pillar_1.4.4        RSQLite_2.2.0       lattice_0.20-41     glue_1.4.0          downloader_0.4      digest_0.6.25      
+[25] RColorBrewer_1.1-2  polyclip_1.10-0     qvalue_2.20.0       colorspace_1.4-1    cowplot_1.0.0       Matrix_1.2-18      
+[31] plyr_1.8.6          pkgconfig_2.0.3     purrr_0.3.4         GO.db_3.11.1        scales_1.1.0        ggplotify_0.0.5    
+[37] europepmc_0.3       tweenr_1.0.1        enrichplot_1.8.1    BiocParallel_1.22.0 ggforce_0.3.1       tibble_3.0.1       
+[43] farver_2.0.3        ggplot2_3.3.0       ellipsis_0.3.0      magrittr_1.5        crayon_1.3.4        memoise_1.1.0      
+[49] MASS_7.3-51.5       xml2_1.3.2          tools_4.0.0         data.table_1.12.8   prettyunits_1.1.1   hms_0.5.3          
+[55] lifecycle_0.2.0     stringr_1.4.0       munsell_0.5.0       compiler_4.0.0      gridGraphics_0.5-0  rlang_0.4.6        
+[61] ggridges_0.5.2      grid_4.0.0          rstudioapi_0.11     igraph_1.2.5        labeling_0.3        gtable_0.3.0       
+[67] DBI_1.1.0           reshape2_1.4.4      graphlayouts_0.7.0  R6_2.4.1            gridExtra_2.3       knitr_1.28         
+[73] dplyr_0.8.5         bit_1.1-15.2        fastmatch_1.1-0     fgsea_1.14.0        GOSemSim_2.14.0     stringi_1.4.6      
+[79] Rcpp_1.0.4.6        vctrs_0.2.4         scatterpie_0.1.4    tidyselect_1.0.0    xfun_0.13
