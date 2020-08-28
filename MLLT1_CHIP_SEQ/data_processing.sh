be7ef3a3c1cb1e79d411622fd3b35988ee958a31
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-08-28 15:43:54 +0100

    data processing for ChIP-Seq files - cat

---
+++ MLLT1_CHIP_SEQ/data_processing.sh
@@ -0,0 +1,20 @@
+# cat files separately in each folder in command line, then move into merge folder
+
+# cat MCF-DMSO-24h-input_S9_L001_R1_001.fastq.gz  MCF-DMSO-24h-input_S9_L002_R1_001.fastq.gz  MCF-DMSO-24h-input_S9_L003_R1_001.fastq.gz  MCF-DMSO-24h-input_S9_L004_R1_001.fastq.gz > MCF_DMSO_24H_INPUT_R1.fastq.gz
+# cat MCF-DMSO-24h-input_S9_L001_R2_001.fastq.gz  MCF-DMSO-24h-input_S9_L002_R2_001.fastq.gz  MCF-DMSO-24h-input_S9_L003_R2_001.fastq.gz  MCF-DMSO-24h-input_S9_L004_R2_001.fastq.gz > MCF_DMSO_24H_INPUT_R2.fastq.gz
+# cat MCF-SGC-24h-input_S10_L001_R1_001.fastq.gz  MCF-SGC-24h-input_S10_L002_R1_001.fastq.gz  MCF-SGC-24h-input_S10_L003_R1_001.fastq.gz  MCF-SGC-24h-input_S10_L004_R1_001.fastq.gz > MCF_SGC_24H_INPUT_R1.fastq.gz
+# cat MCF-SGC-24h-input_S10_L001_R2_001.fastq.gz  MCF-SGC-24h-input_S10_L002_R2_001.fastq.gz  MCF-SGC-24h-input_S10_L003_R2_001.fastq.gz  MCF-SGC-24h-input_S10_L004_R2_001.fastq.gz > MCF_SGC_24H_INPUT_R2.fastq.gz
+# cat MCF-DMSO-7d-input_S11_L001_R1_001.fastq.gz  MCF-DMSO-7d-input_S11_L002_R1_001.fastq.gz  MCF-DMSO-7d-input_S11_L003_R1_001.fastq.gz  MCF-DMSO-7d-input_S11_L004_R1_001.fastq.gz > MCF_DMSO_7D_INPUT_R1.fastq.gz
+# cat MCF-DMSO-7d-input_S11_L001_R2_001.fastq.gz  MCF-DMSO-7d-input_S11_L002_R2_001.fastq.gz  MCF-DMSO-7d-input_S11_L003_R2_001.fastq.gz  MCF-DMSO-7d-input_S11_L004_R2_001.fastq.gz > MCF_DMSO_7D_INPUT_R2.fastq.gz
+# cat MCF-SGC-7d-input_S12_L001_R1_001.fastq.gz  MCF-SGC-7d-input_S12_L002_R1_001.fastq.gz  MCF-SGC-7d-input_S12_L003_R1_001.fastq.gz  MCF-SGC-7d-input_S12_L004_R1_001.fastq.gz > MCF_SGC_7D_INPUT_R1.fastq.gz
+# cat MCF-SGC-7d-input_S12_L001_R2_001.fastq.gz  MCF-SGC-7d-input_S12_L002_R2_001.fastq.gz  MCF-SGC-7d-input_S12_L003_R2_001.fastq.gz  MCF-SGC-7d-input_S12_L004_R2_001.fastq.gz > MCF_SGC_7D_INPUT_R2.fastq.gz
+# cat MCF-DMSO-24h-IP_S13_L001_R1_001.fastq.gz  MCF-DMSO-24h-IP_S13_L002_R1_001.fastq.gz  MCF-DMSO-24h-IP_S13_L003_R1_001.fastq.gz  MCF-DMSO-24h-IP_S13_L004_R1_001.fastq.gz > MCF_DMSO_24H_IP_R1.fastq.gz
+# cat MCF-DMSO-24h-IP_S13_L001_R2_001.fastq.gz  MCF-DMSO-24h-IP_S13_L002_R2_001.fastq.gz  MCF-DMSO-24h-IP_S13_L003_R2_001.fastq.gz  MCF-DMSO-24h-IP_S13_L004_R2_001.fastq.gz > MCF_DMSO_24H_IP_R2.fastq.gz
+# cat MCF-SGC-24h-IP_S14_L001_R1_001.fastq.gz  MCF-SGC-24h-IP_S14_L002_R1_001.fastq.gz  MCF-SGC-24h-IP_S14_L003_R1_001.fastq.gz  MCF-SGC-24h-IP_S14_L004_R1_001.fastq.gz > MCF_SGC_24H_IP_R1.fastq.gz
+# cat MCF-SGC-24h-IP_S14_L001_R2_001.fastq.gz  MCF-SGC-24h-IP_S14_L002_R2_001.fastq.gz  MCF-SGC-24h-IP_S14_L003_R2_001.fastq.gz  MCF-SGC-24h-IP_S14_L004_R2_001.fastq.gz > MCF_SGC_24H_IP_R2.fastq.gz
+# cat MCF-DMSO-7d-IP_S15_L001_R1_001.fastq.gz  MCF-DMSO-7d-IP_S15_L002_R1_001.fastq.gz  MCF-DMSO-7d-IP_S15_L003_R1_001.fastq.gz  MCF-DMSO-7d-IP_S15_L004_R1_001.fastq.gz > MCF_DMSO_7D_IP_R1.fastq.gz
+# cat MCF-DMSO-7d-IP_S15_L001_R2_001.fastq.gz  MCF-DMSO-7d-IP_S15_L002_R2_001.fastq.gz  MCF-DMSO-7d-IP_S15_L003_R2_001.fastq.gz  MCF-DMSO-7d-IP_S15_L004_R2_001.fastq.gz > MCF_DMSO_7D_IP_R2.fastq.gz
+# cat MCF-SGC-7d-IP_S16_L001_R1_001.fastq.gz  MCF-SGC-7d-IP_S16_L002_R1_001.fastq.gz  MCF-SGC-7d-IP_S16_L003_R1_001.fastq.gz  MCF-SGC-7d-IP_S16_L004_R1_001.fastq.gz > MCF_SGC_7D_IP_R1.fastq.gz
+# cat MCF-SGC-7d-IP_S16_L001_R2_001.fastq.gz  MCF-SGC-7d-IP_S16_L002_R2_001.fastq.gz  MCF-SGC-7d-IP_S16_L003_R2_001.fastq.gz  MCF-SGC-7d-IP_S16_L004_R2_001.fastq.gz > MCF_SGC_7D_IP_R2.fastq.gz
+
+
