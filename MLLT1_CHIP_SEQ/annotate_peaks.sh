97dbba747a5ed0fbaf1701f5057ed238fc0673e6
LingFelce <55535650+LingFelce@users.noreply.github.com>
2021-10-15 15:29:52 +0100

    use tss properly - remove peak file
    From Nick: If you’re saying ’tss’ you don’t need to provide a peak file (because the tss list is your peak file). The problem is that whatever you write after the peak file it assumes is the genome, so it’s looking for a genome called tss.

--- MLLT1_CHIP_SEQ/annotate_peaks.sh
+++ MLLT1_CHIP_SEQ/annotate_peaks.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 #SBATCH --partition=batch
 #SBATCH --job-name=annotate_peaks
-#SBATCH --ntasks=8
+#SBATCH --ntasks=5
 #SBATCH --mem=25G
 #SBATCH --time=00-00:30:00
 
@@ -15,21 +15,22 @@ module load homer
 module load ucsctools
 
 
-annotatePeaks.pl SUM_MLLT1_DMSO_24H_FACTOR.txt hg38 \
--list sum_shared_genes_7d_down_all.txt \
--d SUM_DMSO_24H_IP/ SUM_SGC_24H_IP/ -size 2000 -hist 100 > results/SUM_MLLT1_24H_down_degs.txt 
+annotatePeaks.pl tss hg38 \
+-list mcf_shared_genes_7d_down_all.txt \
+-d MCF_DMSO_24H_IP/ MCF_SGC_24H_IP/ -size 2000 -hist 100 > results/MCF_MLLT1_24H_down_degs_tss.txt 
 
-annotatePeaks.pl SUM_MLLT1_DMSO_7D_FACTOR.txt hg38 \
--list sum_shared_genes_7d_down_all.txt \
--d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 2000 -hist 100 > results/SUM_MLLT1_7D_down_degs.txt
+annotatePeaks.pl tss hg38 \
+-list mcf_shared_genes_7d_down_all.txt \
+-d MCF_DMSO_7D_IP/ MCF_SGC_7D_IP/ -size 2000 -hist 100 > results/MCF_MLLT1_7D_down_degs_tss.txt
 
-annotatePeaks.pl SUM_MLLT1_DMSO_24H_FACTOR.txt hg38 \
--list sum_shared_genes_7d_up_all.txt \
--d SUM_DMSO_24H_IP/ SUM_SGC_24H_IP/ -size 2000 -hist 100 > results/SUM_MLLT1_24H_up_degs.txt 
+annotatePeaks.pl tss hg38 \
+-list mcf_shared_genes_7d_up_all.txt \
+-d MCF_DMSO_24H_IP/ MCF_SGC_24H_IP/ -size 2000 -hist 100 > results/MCF_MLLT1_24H_up_degs_tss.txt 
 
-annotatePeaks.pl SUM_MLLT1_DMSO_7D_FACTOR.txt hg38 \
--list sum_shared_genes_7d_up_all.txt \
--d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 2000 -hist 100 > results/SUM_MLLT1_7D_up_degs.txt
+annotatePeaks.pl tss hg38 \
+-list mcf_shared_genes_7d_up_all.txt \
+-d MCF_DMSO_7D_IP/ MCF_SGC_7D_IP/ -size 2000 -hist 100 > results/MCF_MLLT1_7D_up_degs_tss.txt
+
 
 
 # same as before but not histogram - want to plot boxplot
@@ -37,7 +38,7 @@ annotatePeaks.pl SUM_MLLT1_DMSO_7D_FACTOR.txt hg38 \
 #!/bin/bash
 #SBATCH --partition=batch
 #SBATCH --job-name=annotate_peaks
-#SBATCH --ntasks=8
+#SBATCH --ntasks=5
 #SBATCH --mem=25G
 #SBATCH --time=00-00:30:00
 
@@ -48,20 +49,19 @@ module load homer
 module load ucsctools
 
 
-annotatePeaks.pl SUM_MLLT1_DMSO_24H_FACTOR.txt hg38 \
+annotatePeaks.pl tss hg38 \
 -list sum_shared_genes_7d_down_all.txt \
--d SUM_DMSO_24H_IP/ SUM_SGC_24H_IP/ -size 1000 > results/SUM_MLLT1_24H_down_degs_bp.txt 
+-d SUM_DMSO_24H_IP/ SUM_SGC_24H_IP/ -size 1000 > results/SUM_MLLT1_24H_down_degs_bp_tss.txt 
 
-annotatePeaks.pl SUM_MLLT1_DMSO_7D_FACTOR.txt hg38 \
+annotatePeaks.pl tss hg38 \
 -list sum_shared_genes_7d_down_all.txt \
--d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 1000 > results/SUM_MLLT1_7D_down_degs_bp.txt
+-d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 1000 > results/SUM_MLLT1_7D_down_degs_bp_tss.txt
 
-annotatePeaks.pl SUM_MLLT1_DMSO_24H_FACTOR.txt hg38 \
+annotatePeaks.pl tss hg38 \
 -list sum_shared_genes_7d_up_all.txt \
--d SUM_DMSO_24H_IP/ SUM_SGC_24H_IP/ -size 1000 > results/SUM_MLLT1_24H_up_degs_bp.txt 
+-d SUM_DMSO_24H_IP/ SUM_SGC_24H_IP/ -size 1000 > results/SUM_MLLT1_24H_up_degs_bp_tss.txt 
 
-annotatePeaks.pl SUM_MLLT1_DMSO_7D_FACTOR.txt hg38 \
+annotatePeaks.pl tss hg38 \
 -list sum_shared_genes_7d_up_all.txt \
--d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 1000 > results/SUM_MLLT1_7D_up_degs_bp.txt
-
+-d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 1000 > results/SUM_MLLT1_7D_up_degs_bp_tss.txt
 
