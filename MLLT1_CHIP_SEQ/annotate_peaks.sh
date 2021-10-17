b6846e57a081765efe4d1f65b697889e8af8503c
LingFelce <55535650+LingFelce@users.noreply.github.com>
2021-10-17 20:01:50 +0100

    normalisation factor for SUM159

--- MLLT1_CHIP_SEQ/annotate_peaks.sh
+++ MLLT1_CHIP_SEQ/annotate_peaks.sh
@@ -1,5 +1,6 @@
 # more metaplots - same as first metaplot but look specifically at genes up/downregulated from combined RNA-Seq results
 # same code for MCF-7, just replace SUM/sum with MCF/mcf
+# normalisation for SUM159, not for MCF-7
 
 #!/bin/bash
 #SBATCH --partition=batch
@@ -30,6 +31,38 @@ annotatePeaks.pl tss hg38 \
 annotatePeaks.pl tss hg38 \
 -list mcf_shared_genes_7d_up_all.txt \
 -d MCF_DMSO_7D_IP/ MCF_SGC_7D_IP/ -size 2000 -hist 100 > results/MCF_MLLT1_7D_up_degs_tss.txt
+
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=annotate_peaks
+#SBATCH --ntasks=5
+#SBATCH --mem=25G
+#SBATCH --time=00-00:30:00
+
+
+cd /stopgap/donglab/ling/t1-data/MLLT1/ChIP-Seq/analyses/
+
+module load homer
+module load ucsctools
+
+
+annotatePeaks.pl tss hg38 -norm 6672871.029 \
+-list sum_shared_genes_7d_down_all.txt \
+-d SUM_DMSO_24H_IP/ SUM_SGC_24H_IP/ -size 2000 -hist 100 > results/SUM_MLLT1_24H_down_degs_tss.txt 
+
+annotatePeaks.pl tss hg38 -norm 7550759.462 \
+-list sum_shared_genes_7d_down_all.txt \
+-d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 2000 -hist 100 > results/SUM_MLLT1_7D_down_degs_tss.txt
+
+annotatePeaks.pl tss hg38 -norm 6672871.029 \
+-list sum_shared_genes_7d_up_all.txt \
+-d SUM_DMSO_24H_IP/ SUM_SGC_24H_IP/ -size 2000 -hist 100 > results/SUM_MLLT1_24H_up_degs_tss.txt 
+
+annotatePeaks.pl tss hg38 -norm 7550759.462 \
+-list sum_shared_genes_7d_up_all.txt \
+-d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 2000 -hist 100 > results/SUM_MLLT1_7D_up_degs_tss.txt
+
+
 
 
 
@@ -64,4 +97,36 @@ annotatePeaks.pl tss hg38 \
 annotatePeaks.pl tss hg38 \
 -list sum_shared_genes_7d_up_all.txt \
 -d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 1000 > results/SUM_MLLT1_7D_up_degs_bp_tss.txt
+
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=annotate_peaks
+#SBATCH --ntasks=5
+#SBATCH --mem=25G
+#SBATCH --time=00-00:30:00
+
+
+cd /stopgap/donglab/ling/t1-data/MLLT1/ChIP-Seq/analyses/
+
+module load homer
+module load ucsctools
+
+
+annotatePeaks.pl tss hg38 -norm 6672871.029 \
+-list sum_shared_genes_7d_down_all.txt \
+-d SUM_DMSO_24H_IP/ SUM_SGC_24H_IP/ -size 1000 > results/SUM_MLLT1_24H_down_degs_bp_tss.txt 
+
+annotatePeaks.pl tss hg38 -norm 7550759.462 \
+-list sum_shared_genes_7d_down_all.txt \
+-d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 1000 > results/SUM_MLLT1_7D_down_degs_bp_tss.txt
+
+annotatePeaks.pl tss hg38 -norm 6672871.029 \
+-list sum_shared_genes_7d_up_all.txt \
+-d SUM_DMSO_24H_IP/ SUM_SGC_24H_IP/ -size 1000 > results/SUM_MLLT1_24H_up_degs_bp_tss.txt 
+
+annotatePeaks.pl tss hg38 -norm 7550759.462 \
+-list sum_shared_genes_7d_up_all.txt \
+-d SUM_DMSO_7D_IP/ SUM_SGC_7D_IP/ -size 1000 > results/SUM_MLLT1_7D_up_degs_bp_tss.txt
+
+
 
